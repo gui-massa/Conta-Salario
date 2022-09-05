37b9905feaee4852d710cb46f378dcc5de545b9e
gui-massa <99214360+gui-massa@users.noreply.github.com>
2022-09-05 12:10:25 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+from operator import attrgetter
+from functools import total_ordering
+
+@total_ordering
+class ContaSalario:
+
+    def __init__(self, codigo):
+        self._codigo = codigo
+        self._saldo = 0
+
+    def __eq__(self, other):
+        if type(other) != ContaSalario:
+            return False
+
+        return self._codigo == other._codigo and self._saldo == other._saldo
+
+    def __lt__(self, other):
+        if self._saldo != other._saldo:
+            return self._saldo < other._saldo
+        else:
+            return self._codigo < other._codigo
+
+    def deposita(self, valor):
+        self._saldo += valor
+
+    def __str__(self):
+        return "[>>Codigo {} Saldo {}<<]".format(self._codigo, self._saldo)
+
+
+# Testando:
+conta_do_guilherme = ContaSalario(17)
+conta_do_guilherme.deposita(500)
+
+conta_da_daniela = ContaSalario(3)
+conta_da_daniela.deposita(1000)
+
+conta_do_paulo = ContaSalario(133)
+conta_do_paulo.deposita(500)
+
+contas = [conta_do_guilherme, conta_da_daniela, conta_do_paulo]
+
+x = conta_do_guilherme >= conta_da_daniela
+print(x)
+
+for conta in sorted(contas):
+    print(conta)
+
+#for funcionando pois a lista está ordenada por ter sido implementado o método __lt__ (lesser than), desta forma o sorted funciona.
+
